2dc5290590193e77bb0db93b5e1422f5113d6949
Ayush Tyagi <86874794+ayushtya9i@users.noreply.github.com>
2024-02-13 15:42:34 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find Missing And Repeating/find-missing-and-repeating.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a, n);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+     
+         Arrays.sort(arr);
+        int a=0,b=1,p=0;
+        for(int i=0;i<n;i++){
+            if(p==arr[i]){
+            a=p;
+            }
+            else{
+                p=arr[i];
+            }
+            while(b==arr[i]) 
+            b++;
+        }
+      return new int[]{a,b}; 
+    }
+}
