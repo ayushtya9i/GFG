65dc44e358a2eb29a010b379d0fbfd9c45b6922e
Ayush Tyagi <86874794+ayushtya9i@users.noreply.github.com>
2024-04-03 23:35:04 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kth common ancestor in BST/kth-common-ancestor-in-bst.java
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d) {
+        data = d;
+        left = right = null;
+    }
+}
+
+public class GFG {
+    static Node buildTree(String str) {
+        // Corner Case
+        if (str.length() == 0 || str.equals('N')) return null;
+        String[] s = str.split(" ");
+
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+
+        // Starting from the second element
+        int i = 1;
+        while (!q.isEmpty() && i < s.length) {
+            // Get and remove the front of the queue
+            Node currNode = q.remove();
+
+            // Get the curr node's value from the string
+            String currVal = s[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the curr node
+                currNode.left = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= s.length) break;
+            currVal = s[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the curr node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                q.add(currNode.right);
+            }
+
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t > 0) {
+            String s = br.readLine().trim();
+            Node root = buildTree(s);
+
+            String[] snums = br.readLine().split(" ");
+            int k = Integer.parseInt(snums[0]);
+            int x = Integer.parseInt(snums[1]);
+            int y = Integer.parseInt(snums[2]);
+
+            Solution T = new Solution();
+            // Call the function with the needed parameters
+            System.out.println(T.kthCommonAncestor(root, k, x, y));
+
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int kthCommonAncestor(Node root, int k, int x, int y) {
+        if (x == y) return -1;
+        List<Integer> list = new ArrayList<>();
+        find(root,list,x,y);
+        if (k > list.size()) return -1;
+        return list.get(list.size() - k);
+    }
+    
+    public static void find(Node node, List<Integer> list,int x, int y){
+        if(node==null) return;
+        
+        list.add(node.data);
+        if(x<node.data&&y<node.data)
+        find(node.left,list,x,y);
+        else if(x>node.data&&y>node.data)
+        find(node.right,list,x,y);
+        
+    return ;
+    }
+}
